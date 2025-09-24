dc34fff5e93664b9f866bb2831ac2d6d21eac4bc
Aigbogun Benjamin <827554@stma.k12.mn.us>
2025-09-24 14:14:09 -0500

    variable practice

---
+++ Practice/2.3-practice.py
@@ -0,0 +1,41 @@
+#2.3 -variables and imput
+print("operations")
+print(2+2) #add
+print(2-2) #subtract
+print(2*2) #multiply
+print(2/2) #divide
+
+print()
+print("data types")
+
+#string        -"in quotations"
+#interger      -a whole number
+#float         -a decimal number
+#character     -a singal glyph
+#boolean       -True or false
+
+print("don't") #string
+print(2)       #interger
+print(3.14)    #float
+print('c')     #character
+print(True)    #boolean
+
+print()
+print("variables")
+
+x=10
+y=50
+potato=7
+
+print(x*y*potato)
+
+#Naming conventions
+lowercase = False
+UPPERCASE = False
+UpperCamelCase = False       #All lowercase, no spaces, capital for new words
+lowerCamelCase = False       #All lowercase, no spaces, capital for new words
+Snake_case = True            #All lowercase, underscore for spaces
+SCREAMING_SNAKE_CASE =False
+
+#Other general rules to naming things
+#1. Concise (short and discriptive)
