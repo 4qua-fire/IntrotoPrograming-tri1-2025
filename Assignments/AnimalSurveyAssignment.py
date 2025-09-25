d22a9ddbe5535fa6d1b7ef7fe160fd59cc159020
Aigbogun Benjamin <827554@stma.k12.mn.us>
2025-09-25 14:10:56 -0500

    started-animal-survey-assignment

---
+++ Assignments/AnimalSurveyAssignment.py
@@ -0,0 +1,10 @@
+#1. what is your favorite animal?
+#2. why are they your favorite?
+#3. how many types of them are there?
+#4. do you own one?
+#5. how big is it?
+#6. would they be better bigger or smaller?
+#7. what color are they?
+#8. where do they live?
+#9. what do they eat?
+#10. how long do they live?
