12a44029942a49da96c5a764f15f568a5dddd7a0
Aigbogun Benjamin <827554@stma.k12.mn.us>
2025-09-29 14:06:43 -0500

    started-SimpleMadLib.py

---
+++ Assignments/SimpleMadLib.py
@@ -0,0 +1,17 @@
+'''
+long long ago in the lands of [place1] ther lived an {the being =[adjective1] [creature1]}.
+this {the being} dispised [noun1] for [noun1] [adverb1] [verb1] with its [noun2].
+the [noun2] was {the being}'s most [adjective2] thing in its collection.
+without [noun2] {the being} would get more [adjective3] that [adjective1].
+when that happens [place1] will merge with [place2] and become the most [adjective4] [propper noun] of them all.
+[verb ending with ing] the [noun3] once and for all.
+
+2 places
+4 adjectives
+1 creature
+3 nouns
+1 propper noun
+1 adverb
+1 verb
+1 verb ending in ing
+'''
