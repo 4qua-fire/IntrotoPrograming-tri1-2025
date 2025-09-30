2d6c4835ec8c141abeae8d31e19b51058a73dffc
BenjaminAigboun28 <827554@mystma.org>
2025-09-30 14:32:28 -0500

    calculate_tax.py

--- Practice/calculate_tax.py
+++ Practice/calculate_tax.py
@@ -1,2 +1,13 @@
-def sales_tax(price):
-    price*8.
+#create a function to calculate tax
+def calculate_tax(item,price,rate):
+    print(f"item: {item}")
+    print(f"tax: {price*rate}")
+    print(f"total cost: {price+price*rate}")
+
+#choose items
+item = "compter" #random item
+price = 300 #random price
+rate = 0.06875 #minnesota tax rate of 6.875%
+
+#run the functions
+calculate_tax(item,price,rate)
