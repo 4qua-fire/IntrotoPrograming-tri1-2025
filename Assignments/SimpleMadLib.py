b29732fd2568c0d21ba584bb539e7fcf14255a97
Aigbogun Benjamin <827554@stma.k12.mn.us>
2025-09-30 13:39:18 -0500

    Fixed-SimpleMadLib.py

--- Assignments/SimpleMadLib.py
+++ Assignments/SimpleMadLib.py
@@ -1,11 +1,12 @@
 '''
 long long ago in the lands of {place1} ther lived a {being = adjective1 + creature1}.
-this {being} dispised {noun1} for {noun1} {adverb1} {verb1} with its {noun2}.
+this {being} dispised {noun1}, for {noun1} liked to {adverb1} {verb1} with its {noun2}.
 the {noun2} was {being}'s most {adjective2} thing in its collection.
 without {noun2} {being} would get more {adjective3} that {adjective1}.
 when that happens {place1} will merge with {place2} and become the most {adjective4} {propper_noun} of them all.
 {verb ending with ing} the {noun3} once and for all."
 '''
+
 #introduction
 print("hello, welcome to my mad lib\nfor it you will need:\n2 places\n4 adjectives\n1 creature\n3 nouns\n1 propper noun\n1 adverb\n1 verb\n1 verb ending in ing\n\nwords")
 
@@ -36,4 +37,4 @@ verbing = input("verb that ends with ing:")
 adverb = input("adverb:")
 
 #finished madlib
-print(f"long long ago in the lands of {place1} there lived a {being}.\nthis {being} despised {noun1} for {noun1} {adverb} {verb} with its {noun2}.\nthe {noun2} was {being}'s most {adjective2} thing in its collection.\nwithout {noun2} {being} would get more {adjective3} that {adjective1}.\nwhen that happens {place1} will merge with {place2} and become the most {adjective4} {propper_noun} of them all\nand {verbing} the {noun3} once and for all.")
+print(f"long long ago in the lands of {place1} there lived a {being}.\nthis {being} despised {noun1} for {noun1} liked {adverb} {verb} with its {noun2}.\nthe {noun2} was {being}'s most {adjective2} thing in its collection.\nwithout {noun2} {being} would get more {adjective3} that {adjective1}.\nwhen that happens {place1} will merge with {place2} and become the most {adjective4} {propper_noun} of them all\n{verbing} the {noun3} once and for all.")
