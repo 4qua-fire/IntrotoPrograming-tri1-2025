812c7b4f6c96fe4d7ab943cd1bf41b757d94e7fa
BenjaminAigboun28 <827554@mystma.org>
2025-10-02 08:37:14 -0500

    Improved-Simple_Calculator.py

--- Assignments/Simple_Calculator.py
+++ Assignments/Simple_Calculator.py
@@ -1,38 +1,43 @@
-print("Calculator by benjamin\noperations:\naddition   subtraction   multiplication   division\nfloor division   exponentiation   modulus")
+mode = "simple calculator" #make sure mode is set to the function that will be run
+print("current mode: " + mode)
+if mode == "simple calculator":
+    print("Calculator by benjamin\n\noperations:\naddition   subtraction   multiplication   division\nfloor division   exponentiation   modulus")
+x = float(input("number 1:"))
+y = float(input("number 2:"))
 
-#operation functions
-def add(x,y):           #add x and y
+#single operation functions
+def add():           #add x and y
     return x+y
-def subtract(x,y):      #subtract y from x
+def subtract():      #subtract y from x
     return x-y          
-def multiply(x,y):      #multiply x and y
+def multiply():      #multiply x and y
     return x*y
-def divide(x,y):        #divide x by y
+def divide():        #divide x by y
     return x/y
-def floor_divide(x,y):  #divide x by y and round down
+def floor_divide():  #divide x by y and round down
     return x//y
-def exponent(x,y):      #take x to the power of y
+def exponent():      #take x to the power of y
     return x**y
-def mod(x,y):           #take the remainder of dividing x by y 
+def mod():           #take the remainder of dividing x by y 
     return x%y
 
-#my calculator is better than yours btw
-def calculator():
+# calculate function allows the User to choose their opperation with an imput
+def simple_calculator():
     operation = input("operation:")
-    x = int(input("number 1:"))
-    y = int(input("number 2:"))
-    if operation=="addition":           #add
-        print(add(x,y))
-    if operation=="subtraction":        #subtract
-        print(subtract(x,y))
-    if operation=="multiplication":     #multiply
-        print(multiply(x,y))
-    if operation=="division":           #divide
-        print(divide(x,y))
-    if operation=="floor division":     #floor divide
-        print(floor_divide(x,y))
-    if operation=="exponentiation":     #exponentiate
-        print(exponent(x,y))
-    if operation=="modulus":            #mod
-        print(mod(x,y))
-calculator()
+    if operation == "addition":           #add
+        print(add())
+    if operation == "subtraction":        #subtract
+        print(subtract())
+    if operation == "multiplication":     #multiply
+        print(multiply())
+    if operation == "division":           #divide
+        print(divide())
+    if operation == "floor division":     #floor divide
+        print(floor_divide())
+    if operation == "exponentiation":     #exponentiate
+        print(exponent())
+    if operation == "modulus":            #mod
+        print(mod())
+
+#run a function here
+simple_calculator() #make sure mode is set to the function that will be run
