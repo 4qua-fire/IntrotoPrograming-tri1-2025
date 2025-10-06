ecd10718b8755d8b32b733e12cdd83198d487881
4qua-fire <827554@mystma.org>
2025-10-06 13:24:55 -0500

    unit2test.py

---
+++ Tests/unit2test.py
@@ -0,0 +1,22 @@
+#1
+word1=input("word 1:")
+word2=input("word 2:")
+word3=input("word 3:")
+print(word1+word2+word3)
+
+#2
+def add_three(a,b,c):
+    print(a+b+c)
+
+int1 = int(input("interger 1:"))
+int2 = int(input("interger 2:"))
+int3 = int(input("interger 3:"))
+
+add_three(int1,int2,int3)
+
+#3
+def data_three():
+    word0=input("word:")
+    interger0=int(input("interger:"))
+    float0= float(input("float:"))
+    print(f"{word0}{interger0+float0}")
