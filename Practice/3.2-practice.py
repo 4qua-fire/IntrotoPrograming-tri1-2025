e96469037d4eef09897abb9c428149725ec14940
4qua-fire <827554@mystma.org>
2025-10-08 13:02:15 -0500

    BooleanQuiz.py

---
+++ Practice/3.2-practice.py
@@ -0,0 +1,18 @@
+#Remember George Boole layed the ground work of boolean algebra worked on logic and math and basis for binary systems
+#you only get true or false in boolean algebra
+#"maybe my wife is  quantum computer" -mr osowski
+
+
+#conditional operators
+3 == 5 #false
+3 >5 #false
+5 >= 5 #true
+
+#logical operators
+1==1 and 2==2 #both must be true to evaluate true
+1==1 or 1==2 #one must be true to evaluate to true
+not 1==1 #inverts true if false false if true
+
+answer = "cube"
+count = 1
+print(answer == "cube" and count <10)
