6387b7a0e7d3cfdd63477337b47794d53e905f6c
4qua-fire <827554@mystma.org>
2025-10-08 14:07:27 -0500

    started-simplequiz.py

--- Assignments/simplequiz.py
+++ Assignments/simplequiz.py
@@ -1,3 +1,72 @@
-score=5
-score=score+1
-print(score)
+print("Welcome to the best quiz ever!")
+score=0
+q1=input("Question 1:\nwhat is the population of New Orleans as of october 8th 2025\n>")
+q2=input("Question 2:\nWho is the prime minister of Italy\n>")
+q3=input("Question 3:\nwhat is the derivitive of 3x^2+3x-2\n>")
+q4=input("Question 4:\nHow many years did the Ottomon Empire rule\n>")
+q5=input("Question 5:\nwhat is the population of New Orleans\n>")
+q6=input("Question 6:\nwhat is the population of New Orleans\n>")
+q7=input("Question 7:\nwhat is the population of New Orleans\n>")
+q8=input("Question 8:\nwhat is the population of New Orleans\n>")
+q9=input("Question 9:\nwhat is the population of New Orleans\n>")
+q10=input("Question 10:\nwhat is the population of New Orleans\n>")
+
+if q1 == "362,701":
+    score=score+1
+    print("question 1: correct")
+else:
+    print(f"question 1: incorrect\nYour answer:{q1} correct answer: 362,701")
+
+if q2 == "Giorgia Meloni":
+    score=score+1
+    print("question 2: correct")
+else:
+    print(f"question 2: incorrect\nYour answer:{q2} correct answer: Giorgia Meloni")
+
+if q3 == "6x+3":
+    score=score+1
+    print("question 3: correct")
+else:
+    print(f"question 3: incorrect\nYour answer:{q3} correct answer: 6x+3")
+
+if q4 == "362,701":
+    score=score+1
+    print("question 4: correct")
+else:
+    print(f"question 4: incorrect\nYour answer:{q4} correct answer: ")
+
+if q5 == "362,701":
+    score=score+1
+    print("question 5: correct")
+else:
+    print(f"question 5: incorrect\nYour answer:{q5} correct answer:")
+
+if q6 == "362,701":
+    score=score+1
+    print("question 6: correct")
+else:
+    print(f"question 6: incorrect\nYour answer:{q6} correct answer:")
+
+if q7 == "362,701":
+    score=score+1
+    print("question 7: correct")
+else:
+    print(f"question 7: incorrect\nYour answer:{q7} correct answer:")
+
+if q8 == "362,701":
+    score=score+1
+    print("question 8: correct")
+else:
+    print(f"question 8: incorrect\nYour answer:{q8} correct answer:")
+
+if q9 == "362,701":
+    score=score+1
+    print("question 9: correct")
+else:
+    print(f"question 9: incorrect\nYour answer:{q9} correct answer:")
+
+if q10 == "362,701":
+    score=score+1
+    print("question 10: correct")
+else:
+    print(f"question 10: incorrect\nYour answer:{q10} correct answer:")
