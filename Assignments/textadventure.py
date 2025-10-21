9ab43b4f7acd16093a344c258b1fee94eef730c3
4qua-fire <827554@mystma.org>
2025-10-21 14:11:25 -0500

    started adventure

---
+++ Assignments/textadventure.py
@@ -0,0 +1,36 @@
+def dashes():   
+    print("\n"+"-"*50)
+
+def event_start():
+    dashes()
+    print("You wake up in a cave\nYou dont know where you are\n")
+    exit = input("Current options:\n1. Stay in the cave\n2. Leave the cave\n>")
+    
+    if exit == "1":
+        print("event_stay()")
+    elif exit == "2":
+        event_leave()
+    elif exit == "wake up":
+        print("awake_death()")
+    else:
+        print("Error: not an option\nPlease type the number of the choice you would like")
+
+def event_cave_return():
+    dashes()
+    print("You returned to the cave\n")
+    exit = input("Current options:\n1. Stay in the cave\n2. Leave the cave\n>")
+    
+    if exit == "1":
+        print("event_stay()")
+    elif exit == "2":
+        event_leave()
+    else:
+        print("Error: not an option\nPlease type the number of the choice you would like")
+
+
+def event_leave():
+    dashes()
+    print("You leave the cave\nYou see that you are in a clearing in a forest")
+    clearing = input("Current options:\n1. return to the cave\n2. continue into the trees\n 3. investigate the trees")
+
+    event_start()
