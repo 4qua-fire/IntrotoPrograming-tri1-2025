ecddc487b4fe627500171afe63164a4ce4cef446
4qua-fire <827554@mystma.org>
2025-10-22 17:30:04 -0500

    10/22/25 5:30 coded

--- Assignments/textadventure.py
+++ Assignments/textadventure.py
@@ -21,7 +21,7 @@ def event_start():
         print("event_stay()")
     elif exit == "2":
         event_leave()
-    elif exit == "wake up":
+    elif exit == "wake up":      #secret option
         print("awake_death()")
     else:
         error()
@@ -44,13 +44,13 @@ def event_leave():
 def event_strait_path():
     dashes()
     print("Ahead of you the path seems to continue endlessly\nThe trees mirror eachother on both sides\nYou hear the sound of Trees break behind you")
-    path = input("Current options:\n1. Continue onward\n2. turn around\n3.leave path\n>")
+    path = input("Current options:\n1. Continue onward\n2. turn around\n3. leave path\n>")
     if path == "1":
-        print("event_loop()")
+        event_loop()
     elif path == "2": 
         text_strait_turnaround()
     elif path == "3":
-        print("event_moon()")
+        event_moon()
     else:
         error()
         event_strait_path()
@@ -65,9 +65,10 @@ def text_strait_turnaround():
         event_strait_path()
 
 def event_loop():
+    global var_loop_count
     dashes()
     print("The path seems to strech endlessly")
-    loop_path = input("Current options:\n1.Continue forward\n2. Turn around\n3. Leave path")
+    loop_path = input("Current options:\n1. Continue forward\n2. Turn around\n3. Leave path\n>")
     if loop_path == "1":
         var_loop_count+= 1
         event_loop()
@@ -75,9 +76,62 @@ def event_loop():
         var_loop_count += 1
         event_loop()
     elif loop_path == "3":
-        pass
+        event_moon()
+    else:
+        error()
+        event_loop()
 
+def event_moon():
+    dashes()
+    print("The moon shines brightly through the trees\nFollowing the path of least resistance you find yourself at a juction")
+    moon_path = input("Current options:\n1. take the left path\n2. take the right path\n>")
+    if moon_path == "1":
+        dashes()
+        print("RETURN ACTIVATED")
+        print(" - "*17)
+        event_chase()
+    if moon_path == "2":
+        print("right")
+    if moon_path == "3":        #secret option
+        print("death")
 
     
+def event_chase():
+    print("it seems left was the wrong choice\ntowering over you is a face with yellow eyes and teeth")
+    chase_action = input("what are you going to do\n>")
+
+    if len(chase_action) > 10:
+        half_dash()
+        print("You were to slow to react\nbefore you knew it everything went black\n\n[Hint: Try entering a shorter message]\n")
+        replay = input("choose an option to continue\n1. Try again from return\n2. Restart story\n>")
+        if replay == "1":
+            dashes()
+            event_chase()
+        if replay == "2":
+            print("\n"*10)
+            event_start()
+   
+    elif "run" in chase_action or "flee" in chase_action or "bolt" in chase_action:
+        print("run away")
+    
+    elif "fight" in chase_action or "attack" in chase_action:
+        print("fight")
+    
+    else:
+        print("In fear of the monster you couldn't think clearly and it killed you\n\n[Hint: Try entering a simple and relevant action]\n")
+        replay = input("choose an option to continue\n1. Try again from return\n2. Restart story\n>")
+        if replay == "1":
+            dashes()
+            event_chase()
+        if replay == "2":
+            print("\n"*10)
+            event_start()
+   
+    
+
+
+if len("run away") == 8:
+    print("run away")
+
 
 event_start()
