f534d3580da4d282709c9640a8061171c991c735
4qua-fire <827554@mystma.org>
2025-10-22 14:06:21 -0500

    coded

--- Assignments/textadventure.py
+++ Assignments/textadventure.py
@@ -1,13 +1,20 @@
-#general functions
+#variable definition
+var_loop_count =0
+
+
+
+#repeated functions
 def dashes():   
-    print("\n"+"-"*50)
-def option_error():
+    print("\n"+"-"*51)
+def half_dash():
+    print("\n"+" - "*17)
+def error():
     print("Error: not an option\nPlease type the number of the choice you would like")
 
 
 def event_start():
     dashes()
-    print("You wake up in a cave\nYou dont know where you are\n")
+    print("You are in a cave\nYou dont know where you are\n")
     exit = input("Current options:\n1. Stay in the cave\n2. Leave the cave\n>")
     
     if exit == "1":
@@ -17,31 +24,60 @@ def event_start():
     elif exit == "wake up":
         print("awake_death()")
     else:
-        option_error()
-
-def event_cave_return():
-    dashes()
-    print("You returned to the cave\n")
-    exit = input("Current options:\n1. Stay in the cave\n2. Leave the cave\n>")
-    
-    if exit == "1":
-        print("event_stay()")
-    elif exit == "2":
-        event_leave()
-    else:
-        print("Error: not an option\nPlease type the number of the choice you would like")
-
+        error()
+        event_start()
 
 def event_leave():
     dashes()
     print("You leave the cave\nYou see that you are in a clearing in a forest")
-    clearing = input("Current options:\n1. return to the cave\n2. continue into the trees\n 3.investigate the trees\n>")
-    if clearing == "1":
-        print(event_cave_return())
+    clearing = input("Current options:\n1. return to the cave\n2. continue into the trees\n3. investigate the trees\n>")
+    if clearing ==  "1":
+        event_start()
     elif clearing == "2":
-        print("into_trees()")
+        event_strait_path()
     elif clearing == "3":
-        print("investigate_trees")
+        print("investigate_trees()")
     else:
-        option_error()
+        error()
+        event_leave()
+
+def event_strait_path():
+    dashes()
+    print("Ahead of you the path seems to continue endlessly\nThe trees mirror eachother on both sides\nYou hear the sound of Trees break behind you")
+    path = input("Current options:\n1. Continue onward\n2. turn around\n3.leave path\n>")
+    if path == "1":
+        print("event_loop()")
+    elif path == "2": 
+        text_strait_turnaround()
+    elif path == "3":
+        print("event_moon()")
+    else:
+        error()
+        event_strait_path()
+
+def text_strait_turnaround():
+    half_dash()
+    print("turning, around you see that the trees behind you have all fallen down\nyou have no way back")
+    if input("1. turn around\n>") == "1":
+        event_strait_path()
+    else:
+        error()
+        event_strait_path()
+
+def event_loop():
+    dashes()
+    print("The path seems to strech endlessly")
+    loop_path = input("Current options:\n1.Continue forward\n2. Turn around\n3. Leave path")
+    if loop_path == "1":
+        var_loop_count+= 1
+        event_loop()
+    elif loop_path == "2":
+        var_loop_count += 1
+        event_loop()
+    elif loop_path == "3":
+        pass
+
+
+    
+
 event_start()
