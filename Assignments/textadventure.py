d27060cb49e3193967f214675942254940bdd396
4qua-fire <827554@mystma.org>
2025-10-23 14:11:15 -0500

    10/23/25 2:12 adventure coding

--- Assignments/textadventure.py
+++ Assignments/textadventure.py
@@ -1,7 +1,6 @@
-#variable definition
+#path 1 variables
 var_loop_count =0
-
-
+chase_monster = False
 
 #repeated functions
 def dashes():   
@@ -91,47 +90,99 @@ def event_moon():
         print(" - "*17)
         event_chase()
     if moon_path == "2":
-        print("right")
-    if moon_path == "3":        #secret option
+        event_chill()
+    if moon_path == "3":       #secret option
         print("death")
+    else:
+        error()
+        event_moon()
 
     
 def event_chase():
     print("it seems left was the wrong choice\ntowering over you is a face with yellow eyes and teeth")
+    global chase_monster
+    chase_monster = True
     chase_action = input("what are you going to do\n>")
 
     if len(chase_action) > 10:
         half_dash()
-        print("You were to slow to react\nbefore you knew it everything went black\n\n[Hint: Try entering a shorter message]\n")
+        print("You were to slow to react\nBefore you knew it everything went black\n\n[Hint: Try entering a shorter message]\n")
         replay = input("choose an option to continue\n1. Try again from return\n2. Restart story\n>")
         if replay == "1":
             dashes()
             event_chase()
-        if replay == "2":
+        elif replay == "2":
             print("\n"*10)
             event_start()
+        else:
+            error()
+            event_chase()
    
     elif "run" in chase_action or "flee" in chase_action or "bolt" in chase_action:
-        print("run away")
+        event_chase_run()
     
     elif "fight" in chase_action or "attack" in chase_action:
-        print("fight")
-    
+        half_dash()
+        print("You attempt to fight the monster\nYou did well considering you were empty handed\nHowever it was all for naught\nBefore all fades to black you hear the beast release a wretched scream and thump onto the earth")
+        replay = input("choose an option to continue\n1. Try again from return\n2. Restart story\n>")
+        if replay == "1":
+            dashes()
+            event_chase()
+        elif replay == "2":
+            print("\n"*10)
+            event_start()   
+        else:
+            error()
+            event_chase()
     else:
+        half_dash()
         print("In fear of the monster you couldn't think clearly and it killed you\n\n[Hint: Try entering a simple and relevant action]\n")
         replay = input("choose an option to continue\n1. Try again from return\n2. Restart story\n>")
         if replay == "1":
             dashes()
             event_chase()
-        if replay == "2":
+        elif replay == "2":
             print("\n"*10)
             event_start()
+        else:
+            error()
+            event_chase()
    
-    
+def event_chase_run():
+    dashes()
+    print("you run away from the monster\n after some running you approch an intersection")
+    runaway_path = input("Current options\n1. Run to the left\n2. Run to the right\n>")
+    if runaway_path == "1":
+        print()
+    if runaway_path == "2":
+        half_dash()
+        print("you ran to the right forgetting that that was the route you came from\nYou reach the strait path and continue to run\nThe moment you start to slow it seems the monster is immediatly behind you\nEvery thing fades to black")
+        replay = input("choose an option to continue\n1. Try again from return\n2. Restart story\n>")
+        if replay == "1":
+            dashes()
+            event_chase()
+        elif replay == "2":
+            print("\n"*10)
+            event_start()
+        else:
+            error()
+            event_chase_run()
 
+def event_chill():
+    dashes()
+    if chase_monster:
+        print("the danger has passed\nBehind you the monster continues running down the other path\nIts shreiks gradually fading behind you")
+    else:
+        print("It is getting cold now\nA mist forms with each breath")
+        chill_action = input("Current options:\n1. Continue onward\n2. attempt to find warmth")
+        if chill_action == "1":
+            "ending 1"
+        elif chill_action == "2":
+            event_warmth()
+        else:
+            error()
+            event_chill()
 
-if len("run away") == 8:
-    print("run away")
-
-
+def event_warmth():
+    print("You manage to find a blanket ")
 event_start()
